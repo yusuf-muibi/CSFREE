9e2073bfd9c80c611a13a11f650525fde26922c1
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-29 21:44:17 +0100

    Day 1

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the Readme file for the CSFREE Repository. 
+This is the repository for a 100 days coding challenge
+Author: Yusuf Muibi
+Day 1: 29-01-2024
---
+++ day_1.py
@@ -0,0 +1,8 @@
+#So, What do we start with?
+#We'll be starting with a refresher on Loops
+#First will be to print odd numbers from 1 to 21
+print("The odd numbers from 1-21 are:")
+for t in range(1, 21):
+    if t % 2 != 0:
+        print(t)
+    
