8ee495b5335fc704f14ec6bdae715367dbd2d056
yusuf-muibi <muibiyusuf@hotmail.com>
2024-01-31 17:51:28 +0100

    Grade calculator, optimized

--- day_2.py
+++ day_2.py
@@ -1,11 +1,8 @@
 #this is a simple grade calculator
 #the fact still remains that i am really tired, but then we have to stay committed
 # we have to push each day, so yeahhh, here's to today
-user_name = input("Hey there! Kindly provide your name: ")
-user_score = input("Thank you, now, please provide your score: ")
-
-name = str(user_name)
-score = float(user_score)
+name = input("Hey there! Kindly provide your name: \n")
+score = float(input("Thank you, now, please provide your score: \n"))
 
 
 if score >= 90:
@@ -19,13 +16,13 @@ elif score >= 60:
 else:
     grade = "F"
 
-"""F-string
+"""F-string"""
 print(f"The grade for the score {score} is {grade}.")
 
 
-Modulus Operator
+"""Modulus Operator"""
 print("The grade for %s for the score %s is: %s" % (name, score, grade))
-"""
+
 
 """F-string"""
 print(f"The grade for {name} for the score {score} is {grade}")
