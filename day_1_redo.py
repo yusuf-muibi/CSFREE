b310e50cec472c9f5911b2d1df41b49de8e8186a
yusuf-muibi <muibiyusuf@hotmail.com>
2024-02-10 00:47:29 +0100

    function

--- day_1_redo.py
+++ day_1_redo.py
@@ -2,21 +2,18 @@
 #as parameters and returns the largest among them. 
 #Don't use any built-in Python functions like max().
 #def find_largest_number(num1, num2, num3):
-number = input("Kindly type in your numbers separated by a comma")
-while True:
-    numbers = [int(t) for t in number.split(',')]
-    if len(numbers) != 3:
-        print("Kindly input three digits")
-    else:
-        print(f"You typed in {numbers}")
-        break
+#def numbers_in_descending_order(num1, num2, num3):
+number = input("Kindly type in your numbers separated by a comma\n")
+
+numbers = [int(t) for t in number.split(',')]
+
 num1 = numbers[0]
 num2 = numbers[1]
 num3 = numbers[2]
-
 if num1 > num2 and num1 > num3:
     if num2 > num3:
         print(f"The order of the inputted number in descending order is {num1}, {num2}, {num3}")
+
     else:
         print(f"The order of the inputted number in descending order is {num1}, {num3}, {num2}")
 elif  num2 > num1 and  num2 > num3:
@@ -33,7 +30,7 @@ elif num1 == num2 == num3:
     print(f"All the numbers are equal i.e., {num1} ,{num2} & {num3}")
     
 
-                  
+                
     
     
 
